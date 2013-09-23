4fb611bc902a6a2c6414cb44fbaf96b33e1ce6c1
gmbandeira <pi@raspberrypi.(none)>
2013-09-23 08:54:40 -0300

    new file:   Source.cpp 	new file:   a.out 	new file:   img.png 	../test1/cap.wmv 	../test1/test1.cpp

---
+++ opencv/test2/Source.cpp
@@ -0,0 +1,26 @@
+/*
+ * Source.cpp
+ *
+ *  Created on: 23/09/2013
+ *      Author: gmbandeira
+ */
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+int main( int argc, const char** argv )
+{
+	VideoCapture cap(0);
+	Mat img;
+	while(1)
+	{
+		cap >> img;
+		blur(img, img, Size(3, 3));
+		Canny(img, img, 1, 3, 3);
+		imwrite("img.png", img);
+	}
+}
